7221106d348426067e8e04f318d3ade4de185a1f
TimRouze <timothe.rouze1@gmail.com>
2022-04-14 15:43:41 +0200

    Added breaks in build functions ifs.

--- assembler.cpp
+++ assembler.cpp
@@ -36,7 +36,7 @@ vector<bool> successor(kmer k_mer, BloomFilter b_filter){
     vector<bool> succs(4,0);
     string next = "ACGT";
     for(int i = 0; i <= 3; i ++){
-        k_mer >> 2;
+        k_mer >>= 2;
         k_mer += (next[i] / 2) % 4;
         if(b_filter.contains(k_mer)){
             succs[i] = 1;
@@ -49,7 +49,7 @@ vector<bool> predecessor(kmer k_mer, BloomFilter b_filter){
     vector<bool> pred(4,0);
     string next = "ACGT";
     for(int i = 0; i <= 3; i ++){
-        k_mer << 2;
+        k_mer <<= 2;
         k_mer += (next[i] / 2) % 4;
         if(b_filter.contains(k_mer)){
             pred[i] = 1;
@@ -60,10 +60,12 @@ vector<bool> predecessor(kmer k_mer, BloomFilter b_filter){
 
 string build_forwards(string current_contig, kmer curr_kmer, BloomFilter index, int k){
     vector<bool> succs = successor(curr_kmer, index);
+    vector<bool>::iterator it;
     if(accumulate(succs.begin(), succs.end(), 0) == 1){
         for(int i = 0; i <= 3; i++){
             if(succs[i] == 1){
                 current_contig += "ACTG"[i];
+                break;
             }
         }
         return build_forwards(current_contig, str2num(current_contig.substr(1,k)), index, k);
@@ -75,10 +77,12 @@ string build_forwards(string current_contig, kmer curr_kmer, BloomFilter index, 
 
 string build_backwards(string current_contig, kmer curr_kmer, BloomFilter index, int k){
     vector<bool> pred = predecessor(curr_kmer, index);
+    vector<bool>::iterator it;
     if(accumulate(pred.begin(), pred.end(), 0) == 1){
         for(int i = 0; i <= 3; i++){
             if(pred[i] == 1){
                 current_contig = "ACTG"[i] + current_contig;
+                break;
             }
         }
         return build_backwards(current_contig, str2num(current_contig.substr(1,k)), index, k); 
@@ -92,7 +96,7 @@ string create_contig(string filename, int k, int h, int s){
     pair<BloomFilter, kmer> res = construct_index(filename, k, h, s);
     kmer curr_kmer = res.second;
     BloomFilter index = res.first;
-
-    string contig = build_backwards(kmer2str(curr_kmer, k), curr_kmer, index, k) + build_forwards(kmer2str(curr_kmer, k), curr_kmer, index, k).substr(k);
+    string contig = build_backwards(kmer2str(curr_kmer, k), curr_kmer, index, k)
+    + build_forwards(kmer2str(curr_kmer, k), curr_kmer, index, k).substr(k);
     return contig;
 }
